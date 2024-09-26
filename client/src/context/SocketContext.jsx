054b9b587a92cb202a12b0971eaee91d6fc74236
sachinkumar33567 <sachin33567@gmail.com>
2024-09-26 14:20:27 +0530

    Socket URL update for deployement

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
   const [onlineUsers, setOnlineUsers] = useState([]);
 
   useEffect(() => {
-    const socket = io("http://localhost:3000", {
+    // on dev http://localhost:3000
+    const socket = io("/", {
       query: { userId: user?._id },
     });
     setSocket(socket);
